6212fddb0d86a23dc52782155ed499bfc25626bc
ValeriyChurlyaev <chyrlyaevva@gmail.com>
2022-09-19 13:24:00 +0300

    1.1.3 (fix_3)

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    private UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
 
 
     public void createUsersTable() {
